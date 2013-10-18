7dc2634385ce735b4ee0e88b664830958a5d5b1c
Martin Vonheim Larsen <martinvl@student.matnat.uio.no>
2013-10-19 01:31:29 +0200

    Added legend to scoreboard

--- frontend/scoreboard/src/ScoreboardView.js
+++ frontend/scoreboard/src/ScoreboardView.js
@@ -10,6 +10,40 @@ module.exports = ScoreboardView;
 ScoreboardView.prototype.setup = function () {
     this.el = document.createElement('div');
     this.el.className = 'scoreboard';
+
+    var legend = document.createElement('div');
+    legend.className = 'legend';
+    this.el.appendChild(legend);
+
+    var rankBox = document.createElement('div');
+    rankBox.className = 'legend_box rank';
+    rankBox.innerHTML = '#';
+    legend.appendChild(rankBox);
+
+    var nameBox = document.createElement('div');
+    nameBox.className = 'legend_box name';
+    nameBox.innerHTML = 'Nickname';
+    legend.appendChild(nameBox);
+
+    var languageBox = document.createElement('div');
+    languageBox.className = 'legend_box language';
+    languageBox.innerHTML = 'Language';
+    legend.appendChild(languageBox);
+
+    var impTimeBox = document.createElement('div');
+    impTimeBox.className = 'legend_box imp_time';
+    impTimeBox.innerHTML = 'Implementation time';
+    legend.appendChild(impTimeBox);
+
+    var runTimeBox = document.createElement('div');
+    runTimeBox.className = 'legend_box run_time';
+    runTimeBox.innerHTML = 'Running time';
+    legend.appendChild(runTimeBox);
+
+    var codeSizeBox = document.createElement('div');
+    codeSizeBox.className = 'legend_box code_size';
+    codeSizeBox.innerHTML = 'Code size';
+    legend.appendChild(codeSizeBox);
 };
 
 ScoreboardView.prototype.setScores = function (scores) {
